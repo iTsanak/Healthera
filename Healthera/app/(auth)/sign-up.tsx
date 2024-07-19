4f01ca5d8871303532ba799068c25f5d61967808
PabloNicolla <nicola_pablo@yahoo.com.br>
2024-07-19 12:28:01 -0400

    Tentative Project Structure

---
+++ Healthera/app/(auth)/sign-up.tsx
@@ -0,0 +1,29 @@
+import { View, Text, TouchableOpacity } from "react-native";
+import React from "react";
+import { ThemedView } from "@/components/ThemedView";
+import { ThemedText } from "@/components/ThemedText";
+import { router } from "expo-router";
+
+type Props = {};
+
+const SignUp = (props: Props) => {
+  return (
+    <ThemedView style={{ flex: 1, justifyContent: "center" }}>
+      <Text>SignUp</Text>
+      <ThemedView
+        style={{ width: "auto", height: 50, backgroundColor: "#00880050" }}
+      >
+        <TouchableOpacity
+          style={{ flex: 1 }}
+          onPress={() => {
+            router.back();
+          }}
+        >
+          <ThemedText>Go back</ThemedText>
+        </TouchableOpacity>
+      </ThemedView>
+    </ThemedView>
+  );
+};
+
+export default SignUp;
