bd565516b574e26ff34696d25a4c26ab6b18f097
PabloNicolla <nicola_pablo@yahoo.com.br>
2024-07-19 14:38:58 -0400

    simple to navbar template

---
+++ Healthera/components/Navigation/simple-top-navbar.tsx
@@ -0,0 +1,32 @@
+import { View, Text, TouchableOpacity, useColorScheme } from "react-native";
+import React from "react";
+import { ThemedText } from "../ThemedText";
+import { Ionicons } from "@expo/vector-icons";
+import { router } from "expo-router";
+import { Colors } from "@/constants/Colors";
+
+type Props = {};
+
+const SimpleTopNavBar = (props: Props) => {
+  const theme = useColorScheme() ?? "light";
+
+  return (
+    <View className="relative w-full flex-row items-center px-2 py-3">
+      <TouchableOpacity
+        className="z-50 place-self-start rounded-full p-2"
+        onPress={() => {
+          router.back();
+        }}
+      >
+        <Ionicons name="chevron-back" size={30} color={Colors[theme].text} />
+      </TouchableOpacity>
+      <View className="absolute left-0 right-0">
+        <ThemedText className="flex-1 text-center text-2xl">
+          New Account
+        </ThemedText>
+      </View>
+    </View>
+  );
+};
+
+export default SimpleTopNavBar;
