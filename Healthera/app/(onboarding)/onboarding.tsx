197a0935c66c67d7e6801889956d59e3769dc08e
PabloNicolla <nicola_pablo@yahoo.com.br>
2024-07-19 12:47:57 -0400

    onboarding temporary nav options

--- Healthera/app/(onboarding)/onboarding.tsx
+++ Healthera/app/(onboarding)/onboarding.tsx
@@ -8,18 +8,45 @@ type Props = {};
 
 const Onboarding = (props: Props) => {
   return (
-    <ThemedView style={{ flex: 1, justifyContent: "center" }}>
+    <ThemedView style={{ flex: 1, justifyContent: "center", rowGap: 10 }}>
       <ThemedText>Onboarding</ThemedText>
       <ThemedView
         style={{ width: "auto", height: 50, backgroundColor: "#00880050" }}
       >
         <TouchableOpacity
-          style={{ flex: 1 }}
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
           onPress={() => {
+            console.log("pressed");
             router.back();
           }}
         >
           <ThemedText>Go back</ThemedText>
+        </TouchableOpacity>
+      </ThemedView>
+      <ThemedView
+        style={{ width: "auto", height: 50, backgroundColor: "#00880070" }}
+      >
+        <TouchableOpacity
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+          onPress={() => {
+            console.log("pressed");
+            router.push("/sign-in");
+          }}
+        >
+          <ThemedText>Sign In</ThemedText>
+        </TouchableOpacity>
+      </ThemedView>
+      <ThemedView
+        style={{ width: "auto", height: 50, backgroundColor: "#00880050" }}
+      >
+        <TouchableOpacity
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+          onPress={() => {
+            console.log("pressed");
+            router.push("/sign-up");
+          }}
+        >
+          <ThemedText>Sign Up</ThemedText>
         </TouchableOpacity>
       </ThemedView>
     </ThemedView>
