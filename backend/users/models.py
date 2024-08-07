2fea2b35cde14a12c4d58ebac2d2fa85e0d7f0e7
PabloNicolla <nicola_pablo@yahoo.com.br>
2024-08-07 13:39:21 -0400

    adding imageURL (profile image) and deviceID for USER model

--- backend/users/models.py
+++ backend/users/models.py
@@ -7,4 +7,6 @@ class User(AbstractUser):
     # Auto-created id field from Django's Model class
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     phone_number = models.CharField(max_length=15, blank=True)
+    imageURL = models.CharField(blank=True, max_length=500)
+    device_id = models.CharField(blank=True, max_length=200)
     dob = models.DateField(null=True, blank=True)
--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,19 +1,25 @@
 from rest_framework import serializers
 from .models import User
 
-class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
 
+class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ['id', 'username', 'email', 'imageURL',
+                  'device_id', 'first_name', 'last_name', 'is_staff']
+        extra_kwargs = {
+            'password': {'write_only': True},
+        }
 
     def create(self, validated_data):
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data.get('email'),
-            password=validated_data['password']
-        )
+        user = User.objects.create_user(**validated_data)
         return user
-    
-    
+
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            if attr == 'password':
+                instance.set_password(value)
+            else:
+                setattr(instance, attr, value)
+        instance.save()
+        return instance
