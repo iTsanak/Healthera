67105684a1decea14796ea76a7a94f4026732655
PabloNicolla <nicola_pablo@yahoo.com.br>
2024-08-07 16:04:42 -0400

    Temporary workaround to prevent duplicated emails

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,6 +1,9 @@
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.throttling import AnonRateThrottle
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework import status
+from .models import User
 
 
 class PublicView(APIView):
@@ -15,3 +18,14 @@ class PrivateView(APIView):
 
     def get(self, request):
         return Response({"message": "This is a private endpoint"})
+
+
+class CheckEmailView(APIView):
+    throttle_classes = [AnonRateThrottle]
+
+    def post(self, request):
+        email = request.data.get('email')
+        if User.objects.filter(email=email).exists():
+            return Response({"message": "Email is already in use.", "code": "IN_USE"}, status=status.HTTP_200_OK)
+        else:
+            return Response({"message": "Email is available.", "code": "AVAILABLE"}, status=status.HTTP_200_OK)
