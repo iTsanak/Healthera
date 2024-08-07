344de23259114be5335980787a4d660ee5256b4f
PabloNicolla <nicola_pablo@yahoo.com.br>
2024-08-07 14:21:06 -0400

    updating core and users

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,33 +1,8 @@
-from django.shortcuts import render
-from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated, AllowAny
-from .serializers import UserSerializer
-from rest_framework_simplejwt.tokens import RefreshToken
 
-class RegisterView(APIView):
-    def post(self, request):
-        serializer = UserSerializer(data=request.data)
-        print(request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-class LogoutView(APIView):
-    permission_classes = (IsAuthenticated,)
 
-    def post(self, request):
-        try:
-            refresh_token = request.data["refresh_token"]
-            token = RefreshToken(refresh_token)
-            token.blacklist()
-            return Response(status=status.HTTP_205_RESET_CONTENT)
-        except Exception as e:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-    
-    
 class PublicView(APIView):
     permission_classes = (AllowAny,)
 
